880f22db8e3849046cf814944aa7f2ebdf694ecb
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-05 00:13:22 +0100

    style: logo icon update

--- src/components/atoms/icons/MenuIcon.tsx
+++ src/components/atoms/icons/MenuIcon.tsx
@@ -5,10 +5,10 @@ interface Props {
   color?: string;
   w?: number;
   h?: number;
-  boxSize?: number;
+  boxSize?: number | object;
 }
 
-export function MenuIcon({ color, w, h, boxSize }: Props) {
+export function MenuIcon({ color = "main.500", w, h, boxSize }: Props) {
   return (
     <Icon as={BiMenuAltLeft} color={color} w={w} h={h} boxSize={boxSize} />
   );
