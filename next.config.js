537ba3ef9e8057250bdfc6a8dfc651e0f3a47471
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-07 21:18:06 +0100

    feat: next/image remote pattern

--- next.config.js
+++ next.config.js
@@ -11,6 +11,14 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
