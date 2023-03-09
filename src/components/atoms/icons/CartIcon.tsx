f3fe663b5b4105d965f2bcee97fb052540658c9e
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-09 21:00:56 +0100

    style: adds icon

--- src/components/atoms/icons/CartIcon.tsx
+++ src/components/atoms/icons/CartIcon.tsx
@@ -1,4 +1,5 @@
 import { HiShoppingCart } from "react-icons/hi";
+import { MdOutlineAddShoppingCart } from "react-icons/md";
 import { Icon } from "@chakra-ui/react";
 
 interface Props {
@@ -6,10 +7,23 @@ interface Props {
   w?: number;
   h?: number;
   boxSize?: number | object;
+  bg?: string;
 }
 
 export function CartIcon({ color, w, h, boxSize }: Props) {
   return (
     <Icon as={HiShoppingCart} color={color} w={w} h={h} boxSize={boxSize} />
+  );
+}
+export function CartAddIcon({ color, w, h, boxSize, bg }: Props) {
+  return (
+    <Icon
+      as={MdOutlineAddShoppingCart}
+      color={color}
+      w={w}
+      h={h}
+      boxSize={boxSize}
+      bg={bg}
+    />
   );
 }
