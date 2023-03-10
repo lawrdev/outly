f3534dc0067ad2824a6473ab1556f4ddfce9b1ec
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-10 23:24:58 +0100

    file added

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+import { cartState } from "@/recoil/atoms";
+
+export function useLocalStorage<T>(
+  storageKey: string,
+  initialValue: T | (() => T)
+) {
+  const [cartStateValue, setCartStateValue] = useRecoilState(cartState);
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(storageKey);
+
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    // else when not in local storage
+    if (typeof initialValue === "function") {
+      return (initialValue as () => T)();
+
+      // b/cz 'return initialValue()' would tsc fail since initialValue can be T | (() => T)
+    } else {
+      // now else type of initialValue is T
+      return initialValue;
+    }
+  });
+
+  // set to local storage
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(storedValue));
+  }, [storageKey, storedValue]);
+
+  return [storedValue, setStoredValue] as [
+    typeof storedValue,
+    typeof setStoredValue
+  ];
+}
