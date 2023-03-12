2d7ee2f00a8ab973d19a882ababa548d85fb189e
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-12 13:27:47 +0100

    exports funcs

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,26 +1,2 @@
-import { db } from "../../firebase.config";
-import { collection, query, getDocs } from "firebase/firestore";
-import { HeaderImagesProp, ProductProp } from "@/utils";
-
-export const getProducts = async () => {
-  let products: ProductProp[] = [];
-  const q = query(collection(db, "products"));
-
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    products.push({ _id: doc.id, ...doc.data() });
-  });
-  //   console.log("products", products);
-  return products;
-};
-
-export const getHeaderImgs = async () => {
-  let imageArr: HeaderImagesProp[] = [];
-  const q = query(collection(db, "headerImages"));
-
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    imageArr.push(doc.data() as HeaderImagesProp);
-  });
-  return imageArr;
-};
+export * from "./cart";
+export * from "./firebase";
