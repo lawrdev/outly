269c9424722913bf7e47aae0116c27030d0aa4e1
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-12 13:26:34 +0100

    removes btn ref

--- src/components/organisms/cartDrawer/index.tsx
+++ src/components/organisms/cartDrawer/index.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import {
   Drawer,
   DrawerBody,
@@ -10,21 +9,15 @@ import {
   useDisclosure,
   Text,
 } from "@chakra-ui/react";
-import { CartButton } from "@/components/atoms/buttons";
+import { CartButton } from "@/components/atoms";
 
 export function CartDrawer() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = useRef<HTMLButtonElement>(null);
 
   return (
     <>
-      <CartButton onOpen={onOpen} ref={btnRef} />
-      <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
+      <CartButton onOpen={onOpen} />
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
