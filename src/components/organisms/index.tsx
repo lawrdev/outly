88353aa8e2a6ad479ebbd1bee82d6f815c761abd
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-15 23:14:07 +0100

    adds exports

--- src/components/organisms/index.tsx
+++ src/components/organisms/index.tsx
@@ -2,3 +2,8 @@ export * from "./cartDrawer";
 export * from "./menuDrawer";
 export * from "./navbar";
 export * from "./header";
+export { HeadComponent as Head } from "./head";
+export * from "./itemPageImages";
+export * from "./itemPageInfo";
+export * from "./itemPageOrder";
+export * from "./footer";
