25e242d3600d852aba3b6a86c4d8f3f365e6e974
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-18 23:09:37 +0100

    feat(rating): adds star-rating

--- src/components/atoms/rating/index.tsx
+++ src/components/atoms/rating/index.tsx
@@ -1,5 +1,42 @@
-import StarRatingComponent from "react-star-rating-component";
+import { Rating as Rater, Star } from "@smastrom/react-rating";
 
-export function Rating({ value }: { value: number }) {
-  return <StarRatingComponent name="cartRating" starCount={5} value={value} />;
+interface RatingProps {
+  value: number;
+  readOnly?: boolean;
+  height?: string;
+  maxWidth?: number;
+  black?: boolean;
+}
+
+const myStyles = {
+  itemShapes: Star,
+  activeFillColor: "#000",
+  inactiveFillColor: "#c7c7c7",
+};
+
+export function Rating({
+  value,
+  readOnly = true,
+  maxWidth = 150,
+  height = "26px",
+  black = false,
+}: RatingProps) {
+  if (black) {
+    return (
+      <Rater
+        style={{ maxWidth, fontSize: "12px", height }}
+        value={value}
+        readOnly={readOnly}
+        itemStyles={myStyles}
+      />
+    );
+  } else {
+    return (
+      <Rater
+        style={{ maxWidth, fontSize: "12px", height }}
+        value={value}
+        readOnly={readOnly}
+      />
+    );
+  }
 }
