6a0baef77cbdfeaac4630ee57e4791cb347110a2
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-18 23:12:08 +0100

    chore: cleans up cart buttons

--- src/components/molecules/cardBody/cart/index.tsx
+++ src/components/molecules/cardBody/cart/index.tsx
@@ -1,6 +1,11 @@
 import { Dispatch, SetStateAction, useEffect } from "react";
 import { RiAddFill, RiSubtractFill } from "react-icons/ri";
-import { CartAddIcon } from "@/components/atoms";
+import {
+  CartAddButton,
+  CartAddIcon,
+  CartSubButton,
+  ProductCardCartButton,
+} from "@/components/atoms";
 import { Box, Text, IconButton, Tooltip, HStack } from "@chakra-ui/react";
 import {
   decreaseItemQuantity,
@@ -39,8 +44,6 @@ export function Cart({ itemID, addToCart, setAddToCart, setIsInCart }: Props) {
 
   return (
     <Box
-      position="relative"
-      zIndex={1}
       display="flex"
       justifyContent="flex-end"
       alignItems="center"
@@ -49,56 +52,16 @@ export function Cart({ itemID, addToCart, setAddToCart, setIsInCart }: Props) {
       <Box>
         {addToCart && cartValue.find((item) => item?.id === itemID) != null ? (
           <HStack spacing={3}>
-            <IconButton
-              aria-label="subtract"
-              icon={<RiSubtractFill size={18} />}
-              colorScheme="sec.1"
-              bg="gray.200"
-              color="gray.500"
-              _hover={{ bg: "gray.300", color: "gray.600" }}
-              variant="ghost"
-              borderRadius="sm"
-              height={4}
-              paddingInline={0}
-              minWidth={7}
-              onClick={() => handleCartSubIconClick(itemID)}
-            />
+            <CartSubButton onClick={() => handleCartSubIconClick(itemID)} />
             <Text fontSize="xl" fontWeight="bold">
               {cartValue.find((item) => item?.id === itemID)
                 ? cartValue.find((item) => item?.id === itemID)?.quantity
                 : 0}
             </Text>
-            <IconButton
-              aria-label="add"
-              boxSize={8}
-              icon={<RiAddFill size={18} />}
-              colorScheme="sec.1"
-              bg="gray.200"
-              color="gray.500"
-              _hover={{ bg: "gray.300", color: "gray.600" }}
-              variant="ghost"
-              borderRadius="sm"
-              height={4}
-              paddingInline={0}
-              minWidth={7}
-              onClick={() => handleCartAddIconClick(itemID)}
-            />
+            <CartAddButton onClick={() => handleCartAddIconClick(itemID)} />
           </HStack>
         ) : (
-          <Tooltip label="Add to cart" placement="left">
-            <IconButton
-              onClick={() => handleCartIconClick(itemID)}
-              position="relative"
-              zIndex={6}
-              bg="backgrounds.4"
-              variant="unstyled"
-              _hover={{ bg: "gray.100" }}
-              py={2}
-              px={2}
-              aria-label="cart"
-              icon={<CartAddIcon color="main.600" boxSize={6} />}
-            />
-          </Tooltip>
+          <ProductCardCartButton onClick={() => handleCartIconClick(itemID)} />
         )}
       </Box>
     </Box>
