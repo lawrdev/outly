586e3a1eb10c620c4e9e8a13be4253fe1716d2c8
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-03-20 22:51:03 +0100

    style: cartbadge color and radius

--- src/components/atoms/cartBadge/index.tsx
+++ src/components/atoms/cartBadge/index.tsx
@@ -12,7 +12,7 @@ export function CartBadge() {
   }, [cart]);
 
   return (
-    <Box bg="main.400" borderRadius="md" px={2} pb={1}>
+    <Box bg="outly.black" borderRadius="50%" px={2} pb={1}>
       <Text fontSize="sm" fontWeight="semibold" color="white" lineHeight="4">
         {count}
       </Text>
