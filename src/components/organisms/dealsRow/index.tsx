4ae1071f47c06a3f79b0bb59344259e5da271f3c
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-04-04 17:08:36 +0100

    adds dealsrow

--- src/components/organisms/dealsRow/index.tsx
+++ src/components/organisms/dealsRow/index.tsx
@@ -1,40 +1,42 @@
 import { ArrowRIcon, SubHeadText } from "@/components/atoms";
-import { cartState } from "@/recoil/atoms";
 import { ProductProp } from "@/utils";
-import { Box, Text, SimpleGrid } from "@chakra-ui/react";
+import { Box, Text, SimpleGrid, HStack, Select } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
-import { useRecoilState } from "recoil";
 import { ProductCard } from "../productCard";
 
 interface Props {
   products: ProductProp[];
 }
+
 export function DealsRow({ products }: Props) {
   return (
     <Box>
-      <Box display="flex" alignItems="center" gap={2} mb={6}>
-        <SubHeadText>Deals just for you</SubHeadText>
-        <Box display="inline-block" userSelect="none">
-          <Link href="/">
-            {" "}
-            <Text
-              fontWeight="medium"
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              className="underline underline-offset-4"
-              _hover={{ color: "brand.600" }}
-              gap={1}
-            >
-              View all <ArrowRIcon boxSize={4} />
-            </Text>
-          </Link>
+      <HStack mb={6}>
+        <Box display="flex" alignItems="center" gap={2}>
+          <SubHeadText>Top picks for you</SubHeadText>
+          <Box display="inline-block" userSelect="none">
+            <Link href="/">
+              {" "}
+              <Text
+                fontWeight="medium"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                className="underline underline-offset-4"
+                _hover={{ color: "brand.600" }}
+                gap={1}
+              >
+                View all <ArrowRIcon boxSize={4} />
+              </Text>
+            </Link>
+          </Box>
         </Box>
-      </Box>
+      </HStack>
+
       <SimpleGrid
         columns={{ base: 1, sm: 2, md: 3, xl: 4 }}
-        spacing={{ base: 9, sm: 6 }}
+        spacingY={8}
+        spacingX={{ base: 9, sm: 6 }}
       >
         {products?.map((product, index) => (
           <Box key={index}>
