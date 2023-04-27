2849e69f21e3a22c1d29504578f548cde78e76b8
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-04-27 21:42:38 +0100

    File added

---
+++ src/components/General/atoms/buttons/dropdown/index.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button,
+} from "@chakra-ui/react";
+import { FiChevronDown } from "react-icons/fi";
+
+interface Props {
+  title: string;
+  list: Array<{
+    label: string;
+    onClick: () => void;
+  }>;
+  changeTitleOnClick?: boolean;
+  variant?: "solid" | "outline" | "ghost";
+  buttonProps?: {};
+}
+
+export function ButtonDropdown(props: Props) {
+  const [newTitle, setNewTitle] = useState(props.title);
+  return (
+    <Menu>
+      <MenuButton
+        as={Button}
+        rightIcon={<FiChevronDown />}
+        variant={props.variant ? props.variant : "outline"}
+        _active={{ variant: props.variant ? props.variant : "outline" }}
+        {...props.buttonProps}
+      >
+        {newTitle}
+      </MenuButton>
+      <MenuList>
+        {props.list.map((item, index) => (
+          <MenuItem
+            key={index}
+            onClick={() => {
+              if (props.changeTitleOnClick) {
+                setNewTitle(item.label);
+              }
+              item.onClick();
+            }}
+          >
+            {item.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+}
