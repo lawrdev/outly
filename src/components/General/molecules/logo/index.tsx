59a277ba9cecc1844d9aef755360fa64a615b600
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-03 01:12:10 +0100

    file added

---
+++ src/components/General/molecules/logo/index.tsx
@@ -0,0 +1,25 @@
+import { LogoIcon } from "@/components/General/atoms";
+import { LogoText } from "@/components/General/atoms";
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+
+export function Logo({ color = "outly.black" }: { color?: string }) {
+  return (
+    <Box width={"fit-content"}>
+      <Link href="/">
+        <Box
+          className="jello-h"
+          position="relative"
+          zIndex={3}
+          cursor="pointer"
+        >
+          <div className="absolute top-0 right-0 -translate-y-2 translate-x-1.5 rotate-12">
+            <LogoIcon boxSize={4} color="brand.600" />
+          </div>
+
+          <LogoText color={color} />
+        </Box>
+      </Link>
+    </Box>
+  );
+}
