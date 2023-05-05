08498de30357e091087006607baac51204427c24
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-05 20:52:33 +0100

    file added

---
+++ src/components/General/organisms/menuDrawer/index.tsx
@@ -0,0 +1,37 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Text,
+} from "@chakra-ui/react";
+import { Hamburger } from "@/components/General/atoms";
+
+export function MenuDrawer() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Hamburger onOpen={onOpen} />
+      <Drawer isOpen={isOpen} placement="bottom" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>HEADER HERE</DrawerHeader>
+
+          <DrawerBody>
+            <Text>BODY HERE</Text>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Text>FOOTER HERE</Text>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
