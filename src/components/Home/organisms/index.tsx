d69350ee1bca1f20e050e50d95dc5562d67d0145
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-09 09:51:16 +0100

    file added

---
+++ src/components/Home/organisms/index.tsx
@@ -0,0 +1,6 @@
+export * from "./bestSellers";
+export * from "./followUs";
+export * from "./hero";
+export * from "./saleEvent";
+export * from "./shopByCategory";
+export * from "./customerReviews";
