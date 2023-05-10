e2cee1a56cf9a813335e7eda262504dcbcc32d46
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-10 01:17:18 +0100

    adds color op to customcheckbox

--- src/components/General/atoms/checkbox/index.tsx
+++ src/components/General/atoms/checkbox/index.tsx
@@ -1,19 +1,46 @@
-import { Checkbox } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Checkbox, HStack, Text } from "@chakra-ui/react";
 import { BiCheckDouble } from "react-icons/bi";
+import { color } from "framer-motion";
 
 interface Props {
   label: string;
   value?: string;
   onChange?: (e: any) => void;
+  onClick?: (color: string) => void;
+  color?: string;
 }
 export function CustomCheckbox(props: Props) {
-  return (
+  const [toggle, setToggle] = useState(false);
+  return !props.color ? (
     <Checkbox
-      icon={<BiCheckDouble />}
+      // icon={<BiCheckDouble />}
       value={props.value}
       onChange={props.onChange}
     >
       {props.label}
     </Checkbox>
+  ) : (
+    <HStack
+      onClick={() => {
+        props.onClick && props.onClick(props.label!);
+      }}
+    >
+      <Box
+        cursor={"pointer"}
+        borderWidth={2}
+        borderRadius={"50%"}
+        p={0.5}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        borderColor={toggle ? "outly.black" : "gray.300"}
+        onClick={() => setToggle(!toggle)}
+      >
+        <Box p={3} bg={props.color} borderRadius={"50%"} />
+      </Box>
+
+      <Text>{props.label}</Text>
+    </HStack>
   );
 }
