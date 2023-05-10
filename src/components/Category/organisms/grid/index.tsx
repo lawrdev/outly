0f6d1360827d1be929755a08ae9aa56d501dc702
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-10 01:16:08 +0100

    file added

--- src/components/Category/organisms/grid/index.tsx
+++ src/components/Category/organisms/grid/index.tsx
@@ -1,11 +1,97 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ButtonBox, ButtonDropdown } from "@/components/General/atoms";
-import { Box, Grid, GridItem, Heading, HStack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import { IoClose } from "react-icons/io5";
+import { FilterAccordion, FilterAccordionSkeleton } from "./filterAccordion";
+import { FilterObjectProps, ItemProp } from "@/utils";
+import { useQuery } from "@tanstack/react-query";
+import { getFilterOptions } from "@/functions/firebase/category";
+import { CardsGrid } from "./cardGrid";
+import { getAllItems } from "@/functions";
 
-export function CategoryGrid() {
+export function CategoryGrid({ isShop }: { isShop?: boolean }) {
+  const [loading, setLoading] = useState(false);
   const [view2Mode, setView2Mode] = useState(false);
   const [hasFilter, setHasFilter] = useState(false);
+  const [allProducts, setAllProducts] = useState<ItemProp[]>([]);
+  const [filterObject, setFilterObject] = useState<FilterObjectProps[]>([
+    {
+      name: "Category",
+      options: [
+        {
+          category: "Mens",
+          noOfItems: 3,
+        },
+      ],
+    },
+    {
+      name: "Brand",
+      options: [{ category: "Adidas" }],
+    },
+    {
+      name: "Color",
+      options: [{ category: "Black", color: "#000" }],
+    },
+    {
+      name: "Size",
+      options: [{ category: "L" }],
+    },
+    {
+      name: "Price",
+      options: [{ price: { category: "low", from: 0, to: 10000 } }],
+    },
+  ]);
+  const [filterValues, setFilterValues] = useState([]);
+
+  // write func that MAP THRPUGH CURR FILTER OPTIONS AND CREATE VALUES
+
+  const toast = useToast();
+
+  const filterOptionsQuery = useQuery(
+    ["get_filter_options"],
+    () => {
+      return getFilterOptions();
+    },
+    {
+      onSuccess: (data) => {
+        setFilterObject(data);
+      },
+      onError: () => {
+        toast({
+          status: "error",
+          title: "Something went wrong",
+        });
+      },
+    }
+  );
+
+  const getProductsQuery = useQuery(
+    ["get_all_items"],
+    () => {
+      return getAllItems();
+    },
+    {
+      onSuccess: (data) => {
+        setAllProducts(data);
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (getProductsQuery.isLoading) {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [getProductsQuery.isLoading]);
 
   return (
     <Box>
@@ -15,9 +101,16 @@ export function CategoryGrid() {
             <Heading as={"h5"} fontWeight={500} fontSize={"22px"} mb={"18px"}>
               Filter
             </Heading>
+
+            {filterOptionsQuery.isLoading ? (
+              <FilterAccordionSkeleton />
+            ) : (
+              <FilterAccordion filterObject={[...filterObject]} />
+            )}
           </Box>
         </GridItem>
-        <GridItem colSpan={8}>
+
+        <GridItem colSpan={8} pl={6}>
           <HStack justifyContent={"space-between"}>
             <Text>{`Showing all results`}</Text>
 
@@ -40,7 +133,14 @@ export function CategoryGrid() {
               <HStack>
                 <ButtonBox
                   isLight={!view2Mode}
-                  onClick={() => setView2Mode(true)}
+                  onClick={() => {
+                    setLoading(true);
+                    setView2Mode(true);
+                    const timeoutId = setTimeout(function () {
+                      setLoading(false);
+                    }, 1000);
+                    // clearTimeout(timeoutId);
+                  }}
                 >
                   <svg
                     width="6"
@@ -69,7 +169,14 @@ export function CategoryGrid() {
 
                 <ButtonBox
                   isLight={view2Mode}
-                  onClick={() => setView2Mode(false)}
+                  onClick={() => {
+                    setLoading(true);
+                    setView2Mode(false);
+                    const timeoutId = setTimeout(function () {
+                      setLoading(false);
+                    }, 1000);
+                    // clearTimeout(timeoutId);
+                  }}
                 >
                   <svg
                     width="10"
@@ -133,6 +240,14 @@ export function CategoryGrid() {
               </Text>
             </HStack>
           ) : null}
+
+          <Box pt={4}>
+            <CardsGrid
+              products={allProducts}
+              isLoading={loading}
+              view2Mode={view2Mode}
+            />
+          </Box>
         </GridItem>
       </Grid>
     </Box>
