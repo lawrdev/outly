ca5988cb0de0ab137fcf42c7d4c85616605f0b4e
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-21 22:12:50 +0100

    feat: adds slide in animation

--- src/components/General/atoms/animations/index.tsx
+++ src/components/General/atoms/animations/index.tsx
@@ -1,6 +1,31 @@
 import React from "react";
 import { useSpring, animated, config } from "@react-spring/web";
+import { Box } from "@chakra-ui/react";
 
+export function SlideIn({
+  children,
+  fullWidth,
+}: {
+  children: React.ReactNode;
+  fullWidth?: boolean;
+}) {
+  const springStyles = useSpring({
+    from: { opacity: 0.7, x: -160 },
+    to: { opacity: 1, x: 0 },
+
+    delay: 0,
+
+    config: config.slow,
+  });
+
+  return (
+    <animated.div
+      style={{ minWidth: fullWidth ? "100%" : "fit-content", ...springStyles }}
+    >
+      {children}
+    </animated.div>
+  );
+}
 export function Appear({
   children,
   fullWidth,
