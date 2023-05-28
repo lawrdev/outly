cd2e0baa6623348d145dbe5d4de8995eac4b5c9e
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-28 22:49:03 +0100

    fix: clears invalid exports

--- src/components/General/organisms/index.tsx
+++ src/components/General/organisms/index.tsx
@@ -1,11 +1,5 @@
-export * from "./menuDrawer";
 export * from "./navbar";
 export * from "./header";
 export * from "./footer";
-export * from "./checkoutCart";
 
 export { HeadComponent as Head } from "./head";
-export * from "./itemPageImages";
-export * from "./itemPageInfo";
-export * from "./itemPageOrder";
-export * from "./itemPageReview";
