431a70f22e2f522dc5931aaf01f3bbe12c3a237e
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-31 00:25:04 +0100

    feat: adds ListAnimate

--- src/components/General/atoms/animations/index.tsx
+++ src/components/General/atoms/animations/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
 import { useSpring, animated, config } from "@react-spring/web";
 import { Box } from "@chakra-ui/react";
 
@@ -48,5 +49,47 @@ export function Appear({
     >
       {children}
     </animated.div>
+  );
+}
+
+// framer-motion
+
+interface Props {
+  children: ReactNode;
+  delay?: number;
+  direction?: "slide" | "up";
+  duration?: number;
+  style?: {};
+}
+export function ListAnimate({
+  children,
+  style,
+  delay = 0,
+  duration = 0.1,
+  direction = "up",
+}: Props) {
+  return (
+    <motion.div
+      initial={
+        direction === "up" ? { opacity: 0.5, y: -50 } : { opacity: 0.5, x: -50 }
+      }
+      animate={direction === "up" ? { opacity: 1, y: 0 } : { opacity: 1, x: 0 }}
+      exit={direction === "up" ? { opacity: 0, y: 50 } : { opacity: 0, x: 50 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20,
+
+        duration: duration,
+        delay: delay,
+      }}
+      style={{
+        width: "100%",
+        transitionTimingFunction: "cubic-bezier(0.645,0.045,0.355,1)",
+        ...style,
+      }}
+    >
+      {children}
+    </motion.div>
   );
 }
