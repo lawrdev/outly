68dae0367d019faa9bbc5f603481ed21d92b5e38
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-05-31 00:25:23 +0100

    feat: adds BoxLoader

--- src/components/General/atoms/loader/index.tsx
+++ src/components/General/atoms/loader/index.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from "react";
 import { Box, CircularProgress, HStack } from "@chakra-ui/react";
 import Image from "next/image";
-import Gif from "./loader.gif";
+import Gif from "./loader2.gif";
 
 export const ProgressLoader = () => {
   return (
@@ -47,6 +48,53 @@ export const Loader = () => {
           height={200}
         />
       </HStack>
+    </Box>
+  );
+};
+
+export const BoxLoader = ({
+  disclosure,
+  children,
+  ...rest
+}: {
+  disclosure: {
+    isOpen: boolean;
+    onOpen: () => void;
+    onClose: () => void;
+    onToggle: () => void;
+    isControlled: boolean;
+    getButtonProps: (props?: any) => any;
+    getDisclosureProps: (props?: any) => any;
+  };
+  children: ReactNode;
+}) => {
+  return (
+    <Box position={"relative"} {...rest}>
+      <Box
+        opacity={disclosure.isOpen ? 1 : 0}
+        visibility={disclosure.isOpen ? "visible" : "hidden"}
+        transition={"opacity 0.35s cubic-bezier(0.645,0.045,0.355,1) 0.2s"}
+        position={"absolute"}
+        top={0}
+        bottom={0}
+        right={0}
+        left={0}
+        zIndex={10}
+        bg={"#00000030"}
+        borderRadius={"sm"}
+      >
+        <HStack justifyContent={"center"} height={"100%"}>
+          <Image
+            alt={"loading"}
+            src={Gif}
+            quality={100}
+            width={70}
+            height={70}
+            // style={{ minWidth: "auto" }}
+          />
+        </HStack>
+      </Box>
+      {children}
     </Box>
   );
 };
