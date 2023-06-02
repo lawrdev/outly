46b6572898946058f61cc7e7f2daa75ad84f246d
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-02 16:28:25 +0100

    adds exports

---
+++ src/components/General/organisms/index.ts
@@ -0,0 +1,7 @@
+export * from "./navbar";
+export * from "./header";
+export * from "./footer";
+export * from "./dragList";
+export * from "./search";
+
+export { HeadComponent as Head } from "./head";
