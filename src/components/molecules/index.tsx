b5fe6551effaff3e1354c6e0cd6c22160a914e91
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-09 19:49:29 +0100

    d:

--- src/components/molecules/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "./logo";
-export * from "./signIn";
-export * from "./breadcrumb";
-export * from "./cardBody";
-export * from "./cardSwiper";
-export * from "./outlyPrime";
-export * from "./itemCard";
