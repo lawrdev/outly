17d9c5dd7e4cc9ca2cf662fefce3d92bc514e0d1
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-12 19:21:22 +0100

    file added

---
+++ src/pages/shop.tsx
@@ -0,0 +1,11 @@
+import { Head } from "@/components/General/organisms";
+import { ShopTemplate } from "@/templates/shop";
+
+export default function Shop() {
+  return (
+    <>
+      <Head pageTitle="Shop | OutlyStore" />
+      <ShopTemplate />
+    </>
+  );
+}
