9df76c4c45adb325c1eea5b5fcf0896c9dc4f635
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-20 15:57:32 +0100

    fix: 404 page icon

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,53 @@
+import { PageWrapper } from "@/components/General/atoms";
+import { Footer, Head, Header } from "@/components/General/organisms";
+import { chakra, Box, Text, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 import React from "react";
+import { BiErrorAlt } from "react-icons/bi";
 
 function Custom404() {
-  return <div>Custom 404</div>;
+  return (
+    <>
+      <Head pageTitle="404| OutlyStore" />
+
+      <PageWrapper bg={"outly.bg"}>
+        <chakra.header bg={"white"} boxShadow={"sm"}>
+          <Header inActive />
+        </chakra.header>
+
+        <VStack
+          minHeight={"calc(100vh - 210px)"}
+          w={"full"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Box className="rotate-xd" mb={"26px"}>
+            <BiErrorAlt fontSize={"112px"} color={"#ddd"} />
+          </Box>
+
+          <Box maxW={"400px"} textAlign={"center"}>
+            <Text mb={1} fontSize={"xl"} fontWeight={500}>
+              Broken link or Something went wrong!
+            </Text>
+            <Text color={"outly.black500"}>
+              Confirm your url or please
+              <Text
+                as={"span"}
+                pl={"6px"}
+                textDecoration={"underline"}
+                textUnderlineOffset={"4px"}
+                _hover={{ color: "outly.main900" }}
+              >
+                <Link href={"/support"}>contact us</Link>
+              </Text>
+            </Text>
+          </Box>
+        </VStack>
+
+        <Footer />
+      </PageWrapper>
+    </>
+  );
 }
 
 export default Custom404;
