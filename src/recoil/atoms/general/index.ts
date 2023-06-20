6b0c662e97cd795d088abb3f55dc192a857ce303
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-20 15:55:34 +0100

    adds apploader

--- src/recoil/atoms/general/index.ts
+++ src/recoil/atoms/general/index.ts
@@ -1,5 +1,11 @@
 import { getUserLocationInfo } from "@/functions";
-import { UserLocationProp } from "@/utils";
+import {
+  CheckoutInfoProp,
+  FilterObjectProps,
+  FilterValueProp,
+  ItemProp,
+  UserLocationProp,
+} from "@/utils";
 import { atom } from "recoil";
 
 let value: UserLocationProp = {
@@ -17,4 +23,74 @@ if (typeof window !== "undefined") {
 export const userLocationState = atom({
   key: "UserCountryState",
   default: value,
+});
+
+// for showing loader in a page by current page
+export const appLoader = atom({
+  key: "app_loader",
+  default: {
+    category: false,
+  },
+});
+
+// for toast
+export const appToastAtom = atom({
+  key: "app_toast",
+  default: {
+    title: "",
+  },
+});
+
+// for checkout
+export const checkoutInfoAtom = atom({
+  key: "checkout_info",
+  default: {
+    billings: {
+      first_name: "",
+      last_name: "",
+      country: "",
+      address: "",
+      city: "",
+      email: "",
+      phone: "",
+      zip_code: null,
+      customer_note: null,
+    },
+    items: [
+      {
+        uid: "",
+        quantity: null,
+        size: "",
+        color: "",
+      },
+    ],
+    total_price: 0,
+    orderID: "",
+    orderTimestamp: 0,
+    isFreeShipping: false,
+    payment_method: "direct_transfer",
+    create_account: false,
+  } as CheckoutInfoProp,
+});
+
+// for shop items
+export const shopItemsAtom = atom({
+  key: "shop_items",
+  default: [] as ItemProp[],
+});
+// for shop filter
+export const shopFiltersAtom = atom({
+  key: "shop_filters",
+  default: [] as FilterObjectProps[],
+});
+// for user filter
+export const userFiltersAtom = atom({
+  key: "user_filters",
+  default: {
+    categories: [],
+    brand: [],
+    color: [],
+    size: [],
+    price: [],
+  } as FilterValueProp,
 });
