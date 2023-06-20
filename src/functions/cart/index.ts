d741727b69dd1f4b2e5f76ca84df0d72702fb78b
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-20 15:56:15 +0100

    feat: findItemCart

--- src/functions/cart/index.ts
+++ src/functions/cart/index.ts
@@ -17,8 +17,6 @@ export const updateCart = (cart: LocalStorageItemProp[]) => {
   localStorage.setItem("outlyCart", JSON.stringify(cart));
 };
 
-// REFRESH AND CLEAR UP SIMLIPER
-
 export const increaseItemQuantity = (id: string) => {
   const cart: LocalStorageItemProp[] = getCart();
 
@@ -65,7 +63,7 @@ export const decreaseItemQuantity = (id: string) => {
   }
 };
 
-export const removeItem = (id: string) => {
+export const removeItemFromCart = (id: string) => {
   const cart: LocalStorageItemProp[] = getCart();
 
   if (cart.find((item) => item?.id === id) == null) {
@@ -74,5 +72,91 @@ export const removeItem = (id: string) => {
     let newCart = cart.filter((item) => item?.id !== id);
     updateCart(newCart);
     return getCart();
+  }
+};
+
+export const findItemInCart = (id: string) => {
+  const cart: LocalStorageItemProp[] = getCart();
+
+  if (cart.find((item) => item?.id === id) != null) {
+    // cartItem = cart.find((item) => item?.id === id)!;
+    return { inCart: true, cart };
+  } else {
+    return { inCart: false, cart };
+  }
+};
+
+// remove/add size and color to cart
+export const addItemSizeToCart = (id: string, size: string) => {
+  const cart: LocalStorageItemProp[] = getCart();
+
+  if (cart.find((item) => item?.id === id) == null) {
+    localStorage.setItem(
+      "outlyCart",
+      JSON.stringify([...cart, { id, quantity: 1, size }])
+    );
+  } else {
+    let newCart = cart.map((item) =>
+      item?.id === id
+        ? {
+            ...item,
+            size,
+          }
+        : item
+    );
+    updateCart(newCart);
+  }
+};
+
+export const addItemColorToCart = (id: string, color: string) => {
+  const cart: LocalStorageItemProp[] = getCart();
+
+  if (cart.find((item) => item?.id === id) == null) {
+    localStorage.setItem(
+      "outlyCart",
+      JSON.stringify([...cart, { id, quantity: 1, color }])
+    );
+  } else {
+    let newCart = cart.map((item) =>
+      item?.id === id
+        ? {
+            ...item,
+            color,
+          }
+        : item
+    );
+    updateCart(newCart);
+  }
+};
+
+export const removeItemSizeFromCart = (id: string, size: string) => {
+  const cart: LocalStorageItemProp[] = getCart();
+
+  if (cart.find((item) => item?.id === id) != null) {
+    let newCart = cart.map((item) =>
+      item?.id === id
+        ? {
+            ...item,
+            size,
+          }
+        : item
+    );
+    updateCart(newCart);
+  }
+};
+
+export const removeItemColorFromCart = (id: string, color: string) => {
+  const cart: LocalStorageItemProp[] = getCart();
+
+  if (cart.find((item) => item?.id === id) != null) {
+    let newCart = cart.map((item) =>
+      item?.id === id
+        ? {
+            ...item,
+            color,
+          }
+        : item
+    );
+    updateCart(newCart);
   }
 };
