325f071443c7a4274bb09a30b0facf2520566b1d
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-20 15:57:12 +0100

    fix: item page

--- src/pages/item/[id].tsx
+++ src/pages/item/[id].tsx
@@ -1,41 +1,11 @@
-import { Head } from "@/components/shared";
-import { getProducts, getSingleProduct } from "@/functions";
+import { Head } from "@/components/General/organisms";
 import { ItemTemplate } from "@/templates";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { ProductProp } from "@/utils";
 
-export const getStaticPaths = async () => {
-  const results = await getProducts();
-  const data = results.map((product) => ({
-    params: {
-      id: product._id,
-    },
-  }));
-
-  return {
-    paths: data,
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps<{
-  product?: ProductProp;
-}> = async (context) => {
-  const data = await getSingleProduct(context.params?.id as string);
-  if (data != null) {
-    return { props: { product: data } };
-  } else {
-    return { notFound: true };
-  }
-};
-
-export default function Product({
-  product,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function Item() {
   return (
     <>
       <Head pageTitle="Item | OutlyStore" />
-      <ItemTemplate product={product as ProductProp} />
+      <ItemTemplate />
     </>
   );
 }
