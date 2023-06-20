741c6c2672375dc388786eb3648897b97207cb1c
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-06-20 15:56:40 +0100

    feat: get social iamges

--- src/functions/firebase/index.ts
+++ src/functions/firebase/index.ts
@@ -1,26 +1,92 @@
 import { db } from "../../../firebase.config";
-import { collection, query, getDocs } from "firebase/firestore";
-import { HeaderImagesProp, ProductProp } from "@/utils";
+import {
+  collection,
+  query,
+  orderBy,
+  where,
+  getDocs,
+  limit,
+} from "firebase/firestore";
+import {
+  CategoryProp,
+  HeroImagesProp,
+  ItemProp,
+  SocialImageProp,
+} from "@/utils";
+import { calcItemRating } from "../general";
 
-export const getProducts = async () => {
-  let products: ProductProp[] = [];
-  const q = query(collection(db, "products"));
+export const getHeroImgs = async () => {
+  let imageArr: HeroImagesProp[] = [];
+  const q = query(collection(db, "heroImages"));
 
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
-    products.push({ _id: doc.id, ...doc.data() });
-  });
-  //   console.log("products", products);
-  return products;
-};
-
-export const getHeaderImgs = async () => {
-  let imageArr: HeaderImagesProp[] = [];
-  const q = query(collection(db, "headerImages"));
-
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    imageArr.push(doc.data() as HeaderImagesProp);
+    imageArr.push(doc.data() as HeroImagesProp);
   });
   return imageArr;
+};
+
+export const getSocialImgs = async () => {
+  let imageArr: SocialImageProp[] = [];
+  const q = query(collection(db, "socialImages"));
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    imageArr.push(doc.data() as SocialImageProp);
+  });
+  return imageArr;
+};
+
+export const getCategories = async () => {
+  let categories: CategoryProp[] = [];
+  const q = query(collection(db, "categoryItems"), orderBy("category"));
+
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    categories.push({ ...doc.data() } as CategoryProp);
+  });
+
+  return categories;
+};
+
+export const getItems = async (categoryFilter?: string) => {
+  let items: ItemProp[] = [];
+
+  if (categoryFilter) {
+    const q = query(
+      collection(db, "items"),
+      orderBy("sort"),
+      where("category", "==", categoryFilter)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      items.push({ _id: doc.id, ...doc.data() } as ItemProp);
+    });
+  } else {
+    const q = query(collection(db, "items"), orderBy("sort"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      items.push({ _id: doc.id, ...doc.data() } as ItemProp);
+    });
+  }
+
+  //   console.log("products", products);
+  return items;
+};
+
+export const getAllItems = async (limitBy?: number): Promise<ItemProp[]> => {
+  let items: ItemProp[] = [];
+
+  const q = query(
+    collection(db, "items"),
+    orderBy("timestamp"),
+    limit(limitBy || 30)
+  );
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    items.push({ _id: doc.id, ...doc.data() } as ItemProp);
+  });
+
+  return items.map((item) => calcItemRating(item));
 };
