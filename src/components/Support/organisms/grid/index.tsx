39ae636ba29669273527e38bdcee83192777a095
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-11 13:18:22 +0100

    fix: support grid responsiveness

--- src/components/Support/organisms/grid/index.tsx
+++ src/components/Support/organisms/grid/index.tsx
@@ -5,13 +5,18 @@ import { SupportInfo } from "../../molecules/info";
 export const SupportGrid = () => {
   return (
     <Box w={"full"} pt={"65px"}>
-      <HStack w={"100%"} alignItems={"flex-start"}>
-        <Box flexBasis={"30%"}>
+      <HStack
+        w={"100%"}
+        spacing={{ base: 0, lg: 5 }}
+        flexWrap={{ base: "wrap", lg: "nowrap" }}
+        alignItems={"flex-start"}
+      >
+        <Box flexBasis={{ base: "100%", lg: "30%" }}>
           <SupportInfo />
         </Box>
         <Box
-          flexBasis={"70%"}
-          pl={"42px"}
+          flexBasis={{ base: "100%", lg: "70%" }}
+          pl={{ base: 0, lg: "42px" }}
           // borderInlineStart={"1px solid"}
           // borderColor={"#ddd"}
         >
