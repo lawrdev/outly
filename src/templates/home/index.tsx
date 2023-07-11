1437b7b2ffb32ac9b522db714496ad7a0aeceaa1
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-11 13:19:39 +0100

    feat: adds contact-us map

--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -1,4 +1,4 @@
-import { Container, PageWrapper } from "@/components/General/atoms";
+import { ButtonBox, Container, PageWrapper } from "@/components/General/atoms";
 import { Footer, Header } from "@/components/General/organisms";
 import {
   BestSellers,
@@ -14,7 +14,9 @@ import {
   ItemProp,
   SocialImageProp,
 } from "@/utils";
-import { Box } from "@chakra-ui/react";
+import { AspectRatio, Box, Button, Heading, HStack } from "@chakra-ui/react";
+import Link from "next/link";
+import { GrLocationPin } from "react-icons/gr";
 
 interface Props {
   heroImages: HeroImagesProp[];
@@ -63,6 +65,49 @@ export function HomeTemplate({
           <Container>
             <FollowUs socialImages={socialImages} />
           </Container>
+        </section>
+
+        <section>
+          <Box w={"full"} mt={"70px"}>
+            <Heading
+              as={"h3"}
+              mb={4}
+              fontSize={{ base: "2xl", xl: "3xl" }}
+              fontWeight={500}
+              display={"inline-flex"}
+              w={"full"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              textAlign={"center"}
+            >
+              Contact us <GrLocationPin />
+            </Heading>
+
+            <AspectRatio
+              mb={8}
+              // boxShadow={"lg"}
+              ratio={16 / 4}
+              minHeight={"300px"}
+            >
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4783.29202224687!2d3.456987552714117!3d6.447067987021546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1689018863207!5m2!1sen!2sng"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </AspectRatio>
+
+            <HStack w={"full"} justifyContent={"center"}>
+              <Button
+                variant={"outline"}
+                px={12}
+                fontWeight={500}
+                textTransform={"uppercase"}
+                fontSize={"sm"}
+              >
+                <Link href={"/support"}>View Contact info</Link>
+              </Button>
+            </HStack>
+          </Box>
         </section>
       </main>
 
--- src/templates/support/index.tsx
+++ src/templates/support/index.tsx
@@ -2,6 +2,7 @@ import { Container, PageWrapper } from "@/components/General/atoms";
 import { Footer, Header } from "@/components/General/organisms";
 import { SupportHero } from "@/components/Support/molecules";
 import { SupportGrid } from "@/components/Support/organisms";
+import { AspectRatio, Box } from "@chakra-ui/react";
 
 export function SupportTemplate() {
   return (
@@ -13,6 +14,16 @@ export function SupportTemplate() {
       <SupportHero />
 
       <main>
+        <Box mb={8}>
+          <AspectRatio ratio={16 / 6} minHeight={"360px"}>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d4783.29202224687!2d3.456987552714117!3d6.447067987021546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1689018863207!5m2!1sen!2sng"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </AspectRatio>
+        </Box>
+
         <Container>
           <SupportGrid />
         </Container>
